447980a701062506c969644c7db9afb893445ede
Jayin Ton <tonjayin@gmail.com>
2017-09-13 21:06:59 +0800

    接入，待测试

--- Lib/Aliyun/Helper.php
+++ Lib/Aliyun/Helper.php
@@ -6,6 +6,9 @@
 
 namespace Sms\Lib\Aliyun;
 
+use Aliyun\Api\Sms\Request\V20170525\SendSmsRequest;
+use Aliyun\Core\DefaultAcsClient;
+use Aliyun\Core\Profile\DefaultProfile;
 use Sms\Lib\BaseHelper;
 
 class Helper extends BaseHelper {
@@ -22,7 +25,51 @@ class Helper extends BaseHelper {
         require_once(dirname(__FILE__) . '/vendor/autoload.php');
 
 
+        // 短信API产品名
+        $product = "Dysmsapi";
 
+        // 短信API产品域名
+        $domain = "dysmsapi.aliyuncs.com";
+
+        // 暂时不支持多Region
+        $region = "cn-hangzhou";
+
+        // 服务结点
+        $endPointName = "cn-hangzhou";
+
+        // 初始化用户Profile实例
+        $profile = DefaultProfile::getProfile($region, $conf['access_id'], $conf['access_key']);
+
+        // 增加服务结点
+        DefaultProfile::addEndpoint($endPointName, $region, $product, $domain);
+
+        // 初始化AcsClient用于发起请求
+        $acsClient = new DefaultAcsClient($profile);
+
+        // 初始化SendSmsRequest实例用于设置发送短信的参数
+        $request = new SendSmsRequest();
+
+        // 必填，设置雉短信接收号码
+        $request->setPhoneNumbers($to);
+
+        // 必填，设置签名名称
+        $request->setSignName($conf['sign']);
+
+        // 必填，设置模板CODE
+        $request->setTemplateCode($conf['template']);
+
+        // 可选，设置模板参数
+        if($param) {
+            $request->setTemplateParam(json_encode($param));
+        }
+
+        // 发起访问请求
+        $acsResponse = $acsClient->getAcsResponse($request);
+
+        // 打印请求结果
+//         var_dump($acsResponse);
+
+         return $acsResponse;
 
     }
 }
