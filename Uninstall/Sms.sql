72d8ea511a260c70526b94ee9bd51e475eb3adfd
Jayin Ton <tonjayin@gmail.com>
2017-09-14 15:23:47 +0800

    fix missing uninstall sql

--- Uninstall/Sms.sql
+++ Uninstall/Sms.sql
@@ -2,3 +2,5 @@ DROP TABLE IF EXISTS `cms_sms_log`;
 DROP TABLE IF EXISTS `cms_sms_operator`;
 DROP TABLE IF EXISTS `cms_sms_alidayu`;
 DROP TABLE IF EXISTS `cms_sms_ucpaas`;
+DROP TABLE IF EXISTS `cms_sms_alisms`;
+DROP TABLE IF EXISTS `cms_sms_aliyun`;
